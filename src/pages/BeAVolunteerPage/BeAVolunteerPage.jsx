6771528756b3693538572c5f3225917193a5cac0
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-11 22:45:46 +0600

    volunteer need details page update is done

--- src/pages/BeAVolunteerPage/BeAVolunteerPage.jsx
+++ src/pages/BeAVolunteerPage/BeAVolunteerPage.jsx
@@ -14,7 +14,7 @@ const BeAVolunteerPage = () => {
   const [volunteerNeedsDetails, setVolunteerNeedsDetails] = useState({});
 
   useEffect(() => {
-    document.title = 'Be a volunteer page | Volunteer management'
+    document.title = "Be a volunteer page | Volunteer management";
     fetchJobData();
   }, [id]);
 
@@ -24,23 +24,22 @@ const BeAVolunteerPage = () => {
     );
     setVolunteerNeedsDetails(data);
   };
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const form = e.target;
-    const thumbnail = form.thumbnail.value 
-    const title = form.title.value 
-    const category = form.category.value 
-    const location = form.location.value 
-    const number = form.number.value
-    const deadline = startDate
-    const description = form.description.value
-    const name = user?.name 
-    const email = user?.email
-    const suggestion = form.suggestion.value
-    const volunteerId = id
-    
+    const thumbnail = form.thumbnail.value;
+    const title = form.title.value;
+    const category = form.category.value;
+    const location = form.location.value;
+    const number = form.number.value;
+    const deadline = startDate;
+    const description = form.description.value;
+    const name = user?.name;
+    const email = user?.email;
+    const suggestion = form.suggestion.value;
+    const volunteerId = id;
+
     if (number === "" || parseInt(number) === 0) {
       return toast.error("No. of volunteers needed cannot be 0 or empty", {
         position: "top-center",
@@ -48,22 +47,22 @@ const BeAVolunteerPage = () => {
     }
 
     const formData = {
-      thumbnail, 
+      thumbnail,
       title,
       category,
-      location, 
+      location,
       name,
       email,
       deadline,
       description,
       suggestion,
-      status:'requested',
+      status: "requested",
       volunteerId,
     };
 
     // make a post request
     try {
-      const {data} = await axios.post(
+      const { data } = await axios.post(
         `${import.meta.env.VITE_API_URL}/request-be-volunteer`,
         formData
       );
@@ -71,7 +70,7 @@ const BeAVolunteerPage = () => {
       toast.success("Be volunteer request is successfully", {
         position: "top-center",
       });
-      console.log(data)
+      console.log(data);
     } catch (error) {
       toast.error("Something went wrong", {
         position: "top-center",
@@ -79,179 +78,181 @@ const BeAVolunteerPage = () => {
     }
   };
   return (
-    <div className="my-12 mx-4 lg:mx-0 bg-[#f857d0] p-3 lg:p-5 rounded-xl">
-      <h3 className="text-center my-3 text-3xl text-white">
-        Be a volunteer page{" "}
-      </h3>
-      <form onSubmit={handleSubmit}>
-        <div className="lg:flex gap-3">
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              Thumbnail
-            </label>
-            <input
-              name="thumbnail"
-              type="text"
-              defaultValue={volunteerNeedsDetails.thumbnail}
-              disabled
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
-          </div>
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              Post Title
-            </label>
-            <input
-              name="title"
-              type="text"
-              defaultValue={volunteerNeedsDetails.title}
-              disabled
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
-          </div>
-        </div>
-        <div className="lg:flex gap-3">
-          <div className="w-full">
-            <div className="flex flex-col gap-2">
-              <label className="text-gray-700" htmlFor="category">
-                Category
+    <div className="max-w-7xl mx-auto bg-white rounded-xl">
+      <div className="mt-12 mx-4 lg:mx-0 p-3 lg:p-5 rounded-xl">
+        <h3 className="text-center my-3 text-3xl text-black font-semibold">
+          Be a volunteer page{" "}
+        </h3>
+        <form onSubmit={handleSubmit}>
+          <div className="lg:flex gap-3">
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                Thumbnail
               </label>
-              <select
-                name="category"
+              <input
+                name="thumbnail"
+                type="text"
+                defaultValue={volunteerNeedsDetails.thumbnail}
                 disabled
-                defaultValue={volunteerNeedsDetails.category}
-                className="border p-2 rounded-md bg-white"
-              >
-                <option value="Healthcare">Healthcare</option>
-                <option value="Education">Education</option>
-                <option value="Social service">Social service</option>
-                <option value="Animal welfare">Animal welfare</option>
-              </select>
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
             </div>
-          </div>
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              Location
-            </label>
-            <input
-              name="location"
-              type="text"
-              defaultValue={volunteerNeedsDetails.location}
-              disabled
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
-          </div>
-        </div>
-        <div className="lg:flex gap-3">
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              No. of volunteers needed
-            </label>
-            <input
-              name="number"
-              type="number"
-              defaultValue={volunteerNeedsDetails.number}
-              disabled
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
-          </div>
-          <div className="w-full">
-            <div className="flex flex-col gap-2 ">
-              <label className="text-gray-700">Deadline</label>
-
-              {/* Date Picker Input Field */}
-              <DatePicker
-                className="border p-2 rounded-md w-full bg-white"
-                selected={startDate}
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                Post Title
+              </label>
+              <input
+                name="title"
+                type="text"
+                defaultValue={volunteerNeedsDetails.title}
                 disabled
-                onChange={(date) => setStartDate(date)}
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
             </div>
           </div>
-        </div>
-        {/* Organizer name and email */}
-        <div className="lg:flex gap-3">
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              Organizer Name
+          <div className="lg:flex gap-3">
+            <div className="w-full">
+              <div className="flex flex-col gap-2">
+                <label className="text-gray-700" htmlFor="category">
+                  Category
+                </label>
+                <select
+                  name="category"
+                  disabled
+                  defaultValue={volunteerNeedsDetails.category}
+                  className="border p-2 rounded-md bg-white"
+                >
+                  <option value="Healthcare">Healthcare</option>
+                  <option value="Education">Education</option>
+                  <option value="Social service">Social service</option>
+                  <option value="Animal welfare">Animal welfare</option>
+                </select>
+              </div>
+            </div>
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                Location
+              </label>
+              <input
+                name="location"
+                type="text"
+                defaultValue={volunteerNeedsDetails.location}
+                disabled
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
+            </div>
+          </div>
+          <div className="lg:flex gap-3">
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                No. of volunteers needed
+              </label>
+              <input
+                name="number"
+                type="number"
+                defaultValue={volunteerNeedsDetails.number}
+                disabled
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
+            </div>
+            <div className="w-full">
+              <div className="flex flex-col gap-2 ">
+                <label className="text-gray-700">Deadline</label>
+
+                {/* Date Picker Input Field */}
+                <DatePicker
+                  className="border p-2 rounded-md w-full bg-white"
+                  selected={startDate}
+                  disabled
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
+            </div>
+          </div>
+          {/* Organizer name and email */}
+          <div className="lg:flex gap-3">
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                Organizer Name
+              </label>
+              <input
+                name="name"
+                type="text"
+                defaultValue={volunteerNeedsDetails.name}
+                disabled
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                Organizer Email
+              </label>
+              <input
+                name="email"
+                type="email"
+                defaultValue={volunteerNeedsDetails.email}
+                disabled
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
+            </div>
+          </div>
+          {/* volunteer and email */}
+          <div className="lg:flex gap-3">
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                Volunteer Name
+              </label>
+              <input
+                name="name"
+                type="text"
+                defaultValue={user?.displayName}
+                disabled
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-gray-700 " htmlFor="job_title">
+                Volunteer Email
+              </label>
+              <input
+                name="email"
+                type="email"
+                defaultValue={user?.email}
+                disabled
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 mt-4">
+            <label className="text-gray-700 " htmlFor="description">
+              Description
             </label>
-            <input
-              name="name"
-              type="text"
-              defaultValue={volunteerNeedsDetails.name}
+            <textarea
+              defaultValue={volunteerNeedsDetails.description}
               disabled
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
+              name="description"
+              id="description"
+            ></textarea>
           </div>
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              Organizer Email
+          <div className="flex flex-col gap-2 mt-4">
+            <label className="text-gray-700 " htmlFor="description">
+              Suggestion
             </label>
-            <input
-              name="email"
-              type="email"
-              defaultValue={volunteerNeedsDetails.email}
-              disabled
+            <textarea
+              placeholder="Suggestion"
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
+              name="suggestion"
+              id="description"
+            ></textarea>
           </div>
-        </div>
-        {/* volunteer and email */}
-        <div className="lg:flex gap-3">
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              Volunteer Name
-            </label>
-            <input
-              name="name"
-              type="text"
-              defaultValue={user?.displayName}
-              disabled
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
+          <div className="flex justify-end mt-6">
+            <button className="disabled:cursor-not-allowed px-8 py-2.5 leading-5 text-white font-semibold text-xl transition-colors duration-300 transhtmlForm bg-green-500 rounded-md hover:bg-green-500 focus:outline-none focus:bg-green-500 w-full">
+              Request
+            </button>
           </div>
-          <div className="w-full">
-            <label className="text-gray-700 " htmlFor="job_title">
-              Volunteer Email
-            </label>
-            <input
-              name="email"
-              type="email"
-              defaultValue={user?.email}
-              disabled
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 mt-4">
-          <label className="text-gray-700 " htmlFor="description">
-            Description
-          </label>
-          <textarea
-            defaultValue={volunteerNeedsDetails.description}
-            disabled
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            name="description"
-            id="description"
-          ></textarea>
-        </div>
-        <div className="flex flex-col gap-2 mt-4">
-          <label className="text-gray-700 " htmlFor="description">
-          Suggestion 
-          </label>
-          <textarea
-            placeholder="Suggestion"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-            name="suggestion"
-            id="description"
-          ></textarea>
-        </div>
-        <div className="flex justify-end mt-6">
-          <button className="disabled:cursor-not-allowed px-8 py-2.5 leading-5 text-black text-xl transition-colors duration-300 transhtmlForm bg-green-500 rounded-md hover:bg-green-500 focus:outline-none focus:bg-green-500 w-full">
-            Request
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/VolunteerNeedDetialsPage/VolunteerNeedDetailsPage.jsx
+++ src/pages/VolunteerNeedDetialsPage/VolunteerNeedDetailsPage.jsx
@@ -22,13 +22,13 @@ const VolunteerNeedDetailsPage = () => {
   };
 
   return (
-    <div className="py-12">
+    <div className="pt-12">
       <div className="bg-white rounded-xl p-4 lg:w-1/2 mx-auto">
         <div className="w-full">
           <img className="rounded-t-xl mb-4 w-full" src={volunteerNeedsDetails.thumbnail} alt="" />
         </div>
         <div className="space-y-1">
-          <h3 className="text-xl font-semibold">Title: {volunteerNeedsDetails.title}</h3>
+          <h3 className="text-xl font-semibold">{volunteerNeedsDetails.title}</h3>
           <h4 className="text-lg">Category: {volunteerNeedsDetails.category}</h4>
           {
             volunteerNeedsDetails?.deadline && <p className="text-lg">Deadline: {format(new Date(volunteerNeedsDetails.deadline), "P")}</p>
@@ -40,7 +40,7 @@ const VolunteerNeedDetailsPage = () => {
           <p>Location: {volunteerNeedsDetails.location}</p>
           <div>
           <Link to={`/beAVolunteer/${id}`}>
-            <button className="text-lg bg-green-500 py-2 px-4 rounded-lg mt-2 text-black">
+            <button className="text-lg bg-green-500 py-2 px-4 rounded-lg mt-2 text-white font-semibold">
             Be a Volunteer
             </button>
           </Link>
